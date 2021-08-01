fd66084893ac91801f49b74669f06db14d36e3d6
Fail Khabibullin <rendelkey@yandex.ru>
2021-08-01 17:26:23 +0300

    Lesson 5 done

---
+++ src/org/fail/learnjavafromsimpletodifficult/lessons/lesson5/VariablesPart1.java
@@ -0,0 +1,35 @@
+package org.fail.learnjavafromsimpletodifficult.lessons.lesson5;
+
+public class VariablesPart1 {
+    public static void main(String[] args) {
+        double myNewDouble = 23.56;
+        double myDoubleResult = myNewDouble / 3;
+
+//        System.out.println("myDoubleResult = " + myDoubleResult);
+
+        // Chars
+        char char1 = 'A';
+        char char2 = 'b';
+        char char3 = '\n';
+        char char4 = '*';
+
+        char char5 = '\u00A9';
+
+      /*  System.out.println("Characters");
+        System.out.println("char1 = " + char1);
+        System.out.println("char2 = " + char2);
+        System.out.println("char3 = " + char3);
+        System.out.println("char4 = " + char4);
+        System.out.println("char5 = " + char5);*/
+
+//        System.out.println("\n" + char1 + char2 + char3 + char4 + char5);
+
+        // Boolean
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        String s = "same string" + "some other string";
+        System.out.println(s);
+    }
+}
